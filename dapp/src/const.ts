12fcaadfed28c686da6c224143733746ba85b323
yenicelik <david.yenicelik@gmail.com>
2021-11-23 13:42:48 +0100

    need to clean up the branches a bit again, will deploy manually with the stupid heroku CLI

--- dapp/src/const.ts
+++ dapp/src/const.ts
@@ -4,3 +4,5 @@ import {PublicKey} from "@solana/web3.js";
 export const PROGRAM_ID: PublicKey = new PublicKey(
     'GGoMTmrJtapovtdjZLv1hdbgZeF4pj8ANWxRxewnZ35g'
 );
+
+export const BACKEND_URL = String(process.env.REACT_APP_BACKEND_URL);

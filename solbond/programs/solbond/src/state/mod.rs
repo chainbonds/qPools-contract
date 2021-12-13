de1e095285c1be27729fd60b14bafd636046399b
yenicelik <david.yenicelik@gmail.com>
2021-12-13 10:36:34 +0100

    will upload the state, should cover both for swaps and liquidity

--- solbond/programs/solbond/src/state/mod.rs
+++ solbond/programs/solbond/src/state/mod.rs
@@ -1,5 +1,5 @@
 pub mod bond_pool_account;
-pub mod invariant_pool_meta;
+pub mod invariant_pool_account;
 
 pub use bond_pool_account::*;
-pub use invariant_pool_meta::*;
+pub use invariant_pool_account::*;

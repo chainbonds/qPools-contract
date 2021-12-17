79cde4b12314264dd9b74fe0fc997b3573add3db
yenicelik <david.yenicelik@gmail.com>
2021-12-17 14:21:45 +0100

    will port to nextjs at a later point in time, right now is too much work lol

--- dapp/tailwind.config.js
+++ dapp/tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/forms'),
+  ],
 }

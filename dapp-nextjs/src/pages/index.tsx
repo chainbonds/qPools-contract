c732ea56a0a511310720f97272816de10a023802
yenicelik <david.yenicelik@gmail.com>
2021-12-18 21:53:04 +0100

    inspectlet

--- dapp-nextjs/src/pages/index.tsx
+++ dapp-nextjs/src/pages/index.tsx
@@ -18,13 +18,25 @@ const Home: NextPage = (props) => {
                     src="https://www.googletagmanager.com/gtag/js?id=G-P5225TV5V8"
                     strategy="afterInteractive"
                 />
-                <Script  id="google-analytics" strategy="afterInteractive">
+                <Script id="google-analytics" strategy="afterInteractive">
                     {`
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
     
                         gtag('config', 'G-P5225TV5V8');
+                    `}
+                </Script>
+                {/*// <!-- Begin Inspectlet Asynchronous Code -->*/}
+                <Script type="text/javascript">
+                    {` 
+                            (function() {
+                            window.__insp = window.__insp || [];
+                            __insp.push(['wid', 171188381]);
+                            var ldinsp = function(){
+                            if(typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=171188381&r=' + Math.floor(new Date().getTime()/3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); };
+                            setTimeout(ldinsp, 0);
+                        })();
                     `}
                 </Script>
             </Head>

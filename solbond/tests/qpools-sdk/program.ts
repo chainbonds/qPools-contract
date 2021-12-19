dae36c8a266d19170b94ec2ef3244af623c418a4
Pouya <devpouya@gmail.com>
2021-12-19 13:59:30 +0100

    program

---
+++ solbond/tests/qpools-sdk/program.ts
@@ -0,0 +1,35 @@
+import * as anchor from "@project-serum/anchor";
+import {clusterApiUrl, Connection} from "@solana/web3.js";
+import {Provider} from "@project-serum/anchor";
+
+// Not sure if this import is allowed...
+//@ts-ignore
+import _invariant_idl from './../../deps/invariant/target/idl/amm.json';
+const invariant_idl: any = _invariant_idl;
+//@ts-ignore
+import _solana_idl from './../../target/idl/solbond.json';
+const solana_idl: any = _solana_idl;
+
+export const getSolbondProgram = (connection: Connection, provider: Provider) => {
+
+    const programId = new anchor.web3.PublicKey("GGoMTmrJtapovtdjZLv1hdbgZeF4pj8ANWxRxewnZ35g");
+    const program = new anchor.Program(
+        solana_idl,
+        programId,
+        provider,
+    );
+
+    return program;
+}
+
+export const getInvariantProgram = (connection: Connection, provider: Provider) => {
+
+    const programId = new anchor.web3.PublicKey("R9PatsTac3Y3UpC7ihYMMgzAQCe1tXnVvkSQ8DtLWUc");
+    const program = new anchor.Program(
+        solana_idl,
+        programId,
+        provider,
+    );
+
+    return program;
+}

c85d8c4d2327b4f54372b5e49e3dcf9c734e6a3f
devpouya <devpouya@gmail.com>
2022-03-31 16:39:29 +0200

    first actual test passed (create portfolio)

--- solbond/programs/solbond/src/tests/instruction_tests.rs
+++ solbond/programs/solbond/src/tests/instruction_tests.rs
@@ -15,36 +15,14 @@ use crate::entry;
 //use solbond::accounts::{SavePortfolio};
 use crate::utils::seeds;
 use anchor_spl::token::{Token};
+use anchor_lang::InstructionData;
 
 
-#[tokio::test]
-async fn test_health_check() {
-    let program_id = Pubkey::new_unique();
-    let (mut banks_client, payer, recent_blockhash) = ProgramTest::new(
-        "solbond",
-        program_id,
-        processor!(solana_test_healthcheck),
-    )
-    .start()
-    .await;
-
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction {
-            program_id,
-            accounts: vec![AccountMeta::new(payer.pubkey(),false)],
-            data: vec![1,2,3],
-        }],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer], recent_blockhash);
-
-    assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
-}
 
 
 #[tokio::test]
 async fn test_create_portfolio() {
-    let program_id = Pubkey::new_unique();
+    let program_id = crate::id();//crate::solbond::ID;//Pubkey::new_unique();
     let mut test = ProgramTest::new(
         "solbond",
         program_id,
@@ -52,19 +30,11 @@ async fn test_create_portfolio() {
     );
     let (mut banks_client, payer, recent_blockhash) = test.start().await;
     let owner = Pubkey::new_unique();
-    let seeds: &[&[u8]] = &[&owner.as_ref(), seeds::PORTFOLIO_SEED];
+    let seeds: &[&[u8]] = &[&payer.pubkey().to_bytes(), seeds::PORTFOLIO_SEED];
     let (portfolio_acc_pbk, portfolio_bump) = Pubkey::find_program_address(seeds, &program_id);
 
-    let save_portfolio_accs = vec![
-        AccountMeta::new_readonly(owner, true),
-        AccountMeta::new(portfolio_acc_pbk, false),
-        AccountMeta::new_readonly(solana_sdk::system_program::id(),false),
-        AccountMeta::new_readonly(spl_token::id(), false),
-        AccountMeta::new_readonly(solana_program::sysvar::rent::id(),false)
-
-    ];
     let anc_acc = crate::accounts::SavePortfolio{
-        owner: owner,
+        owner: payer.pubkey(),
         portfolio_pda: portfolio_acc_pbk,
         system_program: solana_sdk::system_program::id(),
         token_program: Token::id(),
@@ -79,23 +49,21 @@ async fn test_create_portfolio() {
         _sum_of_weights: sumofweights,
         _num_positions: numpos,
         _num_currencies: numcurre
+    }.data();
+    let ix = Instruction {
+        program_id: program_id,
+        accounts: anc_acc,
+        data: data
     };
-    //let create_portfolio_ix = Instruction {
-    //    program_id: program_id,
-    //    accounts: save_portfolio_accs,
-    //    data: &entrypoint::instructions::approve::approve_portfolio_weights::handler(
-    //    )
-    //}
-
-    // let save_portfolio_ctx = SavePortfolio {
-    //     owner:owner,
-    //     portfolio_pda: portfolio_acc_pbk,
-    //     system_program: &solana_sdk::system_program::id(),
-    //     token_program: &spl_token::ID,
-    //     rent: &Rent::get()?,
-    // };
 
 
-   
+   let mut transaction = Transaction::new_with_payer(
+        &[ix],
+        Some(&payer.pubkey()),
+    );
+    transaction.sign(&[&payer], recent_blockhash);
+    assert_matches!(banks_client.process_transaction(transaction).await, Ok(()));
+
+
 
 }

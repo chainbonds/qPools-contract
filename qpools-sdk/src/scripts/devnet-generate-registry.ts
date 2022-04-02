263aa3f8ff14480728a38f193396f4dde515db2b
yenicelik <david.yenicelik@gmail.com>
2022-04-02 19:09:51 +0200

    re-write some parts of the registry. will need to check if more issues arise.

---
+++ qpools-sdk/src/scripts/devnet-generate-registry.ts
@@ -0,0 +1,40 @@
+/**
+ * Script to generate the token lists and the pool lists
+ *
+ * We assume that every pool has an LP token.
+ * This is not true in practice, but we again can have an aritificial LP token per protocol
+ *
+ */
+
+/**
+ * Take the Token List from the SPL Library, and append it by pyth oracles
+ */
+const createTokenList = () => {
+
+}
+
+const createTokenListSolend = () => {
+
+}
+
+/**
+ * For devnet, we create a new one
+ */
+
+const createTokenListMarinade = () => {
+
+}
+
+/**
+ * This is literally the SPL token list. We can just take it from there
+ */
+const createTokenListSaber = () => {
+
+}
+
+/**
+ * Take the Pool List from
+ */
+const createPoolList = () => {
+
+}

aa64a3218f0cc1add8b1ba0e3f171fd0b5d8e6e2
devpouya <devpouya@gmail.com>
2022-04-12 15:26:45 +0200

    saber lp price oracle

--- qpools-sdk/src/instructions/api/saber.ts
+++ qpools-sdk/src/instructions/api/saber.ts
@@ -3,7 +3,8 @@ import {DEV_POOLS_INFO_SABER} from "../../registry/devnet/saber/pools-info.devne
 import {Protocol, ProtocolType} from "../../types/PositionInfo";
 import {ExplicitToken} from "../../types/ExplicitToken";
 import {ExplicitPool} from "../../types/ExplicitPool";
-
+import {Connection, PublicKey, Transaction, TransactionInstruction} from "@solana/web3.js";
+import * as spl from 'easy-spl'
 /**
  * Gotta just copy it from the registry ....
  */
@@ -21,3 +22,36 @@ export const getSaberPools = async  (): Promise<ExplicitPool[]> => {
     });
     return saberPoolList;
 }
+
+
+export const getSaberLpTokens = async (): Promise<ExplicitToken[]> => {
+    let saberTokenList: ExplicitToken[] = DEV_POOLS_INFO_SABER["lpToken"];
+    return saberTokenList;
+}
+
+export const saberPoolTokenToSwap = async (): Promise<any> => {
+ 
+    let map = DEV_POOLS_INFO_SABER["swap"]["state"]["poolTokenMint"].map((x,i) => [
+        x,DEV_POOLS_INFO_SABER["swap"][i]
+    ])
+    return map
+
+}
+
+
+
+export async function saberPoolTokenPrice(connection: Connection, lpMint: PublicKey): Promise<number> {
+    // map pool address to exchange 
+    const thisSwap = await saberPoolTokenToSwap[lpMint.toString()]
+    const token_reserve_a = thisSwap["state"]["tokenA"]["reserve"]
+    const token_reserve_b = thisSwap["state"]["tokenB"]["reserve"]
+    const token_a_bal = await connection.getBalance(new PublicKey(token_reserve_a))
+    const token_b_bal = await connection.getBalance(new PublicKey(token_reserve_b))
+
+    const lp_supply = (await connection.getTokenSupply(lpMint)).value.uiAmount
+    const price = (token_a_bal+ token_b_bal)/lp_supply
+    return price;
+
+}
+
+
